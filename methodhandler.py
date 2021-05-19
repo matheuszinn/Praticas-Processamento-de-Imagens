b3777653c5d5ca979dd8b4f9e563b2b90ef8bd05
Matheuszinn <theustloz@gmail.com>
2021-05-19 13:38:42 -0300

    Modificação na estrutura do dicionáio de métodos, e método execute mais genérico

--- methodhandler.py
+++ methodhandler.py
@@ -1,24 +1,19 @@
 from simpleImage import SimpleImage
 
+
 class MethodHandler:
 
     def __init__(self, image: SimpleImage) -> None:
-        
+
         self.metodos = {
-            "Interpolação": {
-                "Vizinho mais próximo": {
-                    "Ampliação": image.interpolacao_vizinhos_ampliacao,
-                    "Redução": image.interpolacao_vizinhos_reducao
-                },
-                "Bilinear": {
-                    "Ampliação": image.interpolacao_bilinear_ampliacao,
-                    "Redução": image.interpolacao_bilinear_reducao
-                }
-            },
-            "Transformar em cinza": image.in_grayscale
+            "Interpolação": image.interpolação,
+            "Transformar em cinza": image.in_grayscale,
+            "Reflexão/Espelhamento": image.reflexão_espelhamento,
+            "Transformar em negativo": image.negativo
         }
 
-    def execute(self, *vals: str):
-        self.metodos[vals[0]] \
-                    [vals[1]] \
-                    [vals[2]]()
+    def execute(self, options: dict) -> None:
+        if 'opt' in options:
+            self.metodos[options['operation']](options['opt'])
+        else:
+            self.metodos[options['operation']]()
