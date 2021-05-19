f9e995d376723baef1db59fef094b4ae5d37712b
Matheuszinn <theustloz@gmail.com>
2021-05-19 13:37:25 -0300

    Mais limpa]

--- main.py
+++ main.py
@@ -1,62 +1,10 @@
 from methodhandler import MethodHandler
 from PyInquirer import prompt
-from PyInquirer.separator import Separator
 from simpleImage import SimpleImage
-
-questions = [
-    {
-        'type': 'list',
-        'name': 'operation',
-        'message': 'O que deseja fazer com a imagem ?',
-        'choices': [
-            'Interpolação',
-            Separator(),
-            'Transformar em cinza'
-        ]
-    },
-    {
-        'type': 'list',
-        'name': 'interpolationAlgo',
-        'message': 'Por qual método você deseja realizar a interpolação?',
-        'choices': [
-            'Vizinho mais próximo',
-            'Bilinear',
-        ],
-        'when' : lambda x : x['operation'] == 'Interpolação'
-    },
-    {
-        'type': 'list',
-        'name': 'interpolationType',
-        'message': 'Escolha o tipo de interpolação: ',
-        'choices': [
-            'Ampliação',
-            'Redução'
-        ],
-        'when': lambda ans: 'Interpolação' in ans.values()
-    },
-    {
-        'type': 'input',
-        'name': 'imgPath',
-        'message': 'Entre com o nome do arquivo de imagem: '
-    },
-    {
-        'type': 'confirm',
-        'name': 'save',
-        'message': "Você deseja salvar a imagem resultante ?"
-    }
-]
+from utils import QUESTIONS
 
 if __name__ == "__main__":
-    results = prompt(questions)
-
-    image = SimpleImage(results['imgPath'], save_f=results['save'])
+    results = prompt(QUESTIONS)
+    image = SimpleImage(results['imgPath'], results['save'])
     handler = MethodHandler(image)
-
-    if 'Transformar em cinza' in results.values():
-        handler.metodos[results['operation']]()
-    else: 
-        handler.execute(
-            results['operation'],
-            results['interpolationAlgo'],
-            results['interpolationType']
-            )
+    handler.execute(results)
